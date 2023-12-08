3cf31b99109ac04c5aa43b2b7730827a866f03ce
abilio <abiliocharles@gmail.com>
2023-12-08 19:04:51 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home'; // Importação do componente Home
+
+function App() {
+  return (
+    <div className="App">
+      <Home /> {/* Inclusão do componente Home */}
+    </div>
+  );
+}
+
+export default App;
